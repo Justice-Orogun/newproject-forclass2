b0565f8200e7fcd2e84f7a42e9ff2812ba20606a
Justice Orogun <justicemaroorogun@gmail.com>
2023-10-30 18:59:34 +0100

    this is our new project for this week

---
+++ main.js
@@ -0,0 +1,5 @@
+var menuBtn = document.querySelector('.menu-btn'); 
+var menu = document.querySelector('.nav-links'); 
+var menuLinks = document.querySelector('.nav-links li a'); 
+
+menuBtn.addEventListener('click', activeClass); 
